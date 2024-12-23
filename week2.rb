118651e981b3f20a4bf93ed3ed814179ededfea9
Aidan Lowson <dev@aidanlowson.com>
2024-12-23 13:57:06 +0000

    Finishing the grep programme

--- week2.rb
+++ week2.rb
@@ -45,9 +45,103 @@ end
 =end
 
 # Print up to 16 in chunks of 4
+=begin
 i = 0
 while i != 16
   puts "#{i + 1} #{i + 2} #{i + 3} #{i + 4}"
   i = i + 4
 end
+
+=end
+
+# Print 4 numbers at a time
+=begin
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+
+numbers_to_print = []
+
+numbers.each do |number|
+  numbers_to_print.push(number)
+  if number % 4 == 0
+    puts "Next set of 4 are ready, here we go"
+    puts numbers_to_print
+    numbers_to_print = []
+  end
+end
+=end
+
+=begin
+# Create a better tree class
+class Tree
+  attr_accessor :name, :children, :parent
+
+  def initialize(data)
+    if data.is_a?(Hash)
+      # If we get a hash, assume it has exactly one root node
+      root_name, child_hash = data.first
+      @name = root_name
+      @children = []
+      build_tree(child_hash)   
+    else
+      @name = data
+      @children = []
+    end
+    @parent = nil
+  end
+
+  def build_tree(child_hash)
+    child_hash.each do |child_name, grandchild|
+      child = Tree.new(child_name)
+      child.parent = self
+      @children << child
+      child.build_tree(grandchild) unless grandchild.empty?
+    end
+  end
+
+  def print_tree(level = 0)
+    puts " " * level + name
+    children.each { | ch | ch.print_tree(level + 1)}
+  end
+end
+
+family_tree = Tree.new({
+  'grandpa' => { 
+    'dad' => {
+      'child1' => {},
+      'child2' => {}
+    }
+  }
+})
+
+family_tree.print_tree
+=end
+
+# GREP a file
+
+=begin
+  # Write a snippet that takes an input
+  # tests a string against that input
+  # prints out if it can find it or not
+  # 
+  # Read a file
+  # Use same input read
+  # Use same input test
+  # Print out lines where there's a match
+=end
+
+puts 'Type what you want to find...'
+
+string_to_find = gets.chomp
+
+users_regex = /#{string_to_find.to_s}/i
+
+line_number = 1
+File.foreach('./messing_about.txt') do |line|
+  if users_regex.match(line.chomp)
+    puts "Found #{string_to_find} in #{line} - number #{line_number}."
+  end
+  line_number = line_number + 1
+end
+
+puts "Finished checking file"
 
